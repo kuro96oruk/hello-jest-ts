0d34092a7655e464462f8ca33e26162aef08734e
kuro96oruk <kuroki@siba-service.jp>
2023-11-04 15:19:39 +0900

    :sparkles: 2.4.6まで追記

---
+++ src/chapter2/group/concurrent.test.ts
@@ -0,0 +1,19 @@
+//npx jest src/chapter2/group/concurrent.test.ts --runInBand
+//npx jest src/chapter2/group/concurrent.test.ts --runInBand --maxConcurrency=10
+
+const fetchData = () =>
+  new Promise((resolve) => setTimeout(resolve, 1000, "lemon"));
+
+test.concurrent.each(
+  Array.from(new Array(100).keys()).map((n) => ({n, expected: "lemon"}))
+)("fetchData $n", async ({n, expected}) => {
+  console.log(n);
+  await expect(fetchData()).resolves.toBe(expected);
+});
+
+test.concurrent.skip.each(
+  Array.from(new Array(100).keys()).map((n) => ({n, expected: "lemon"}))
+)("concurrent test $n", async ({n, expected}) => {
+  console.log(n);
+  await expect(fetchData()).resolves.toBe(expected);
+});
---
+++ src/chapter2/group/group.test.ts
@@ -0,0 +1,18 @@
+describe("グループ名", () => {
+  test("テストケース1", () => {
+    expect(true).toBe(true);
+  });
+  test("テストケース2", () => {
+    expect(true).toBe(true);
+  });
+  test("テストケース3", () => {
+    expect(true).toBe(true);
+  });
+
+  // 入れ子でグループを定義できる
+  describe("グループ名", () => {
+    test("グループ名", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
