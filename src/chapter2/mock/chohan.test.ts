8dd872807b4812aa7f011d2e0ae96347901417b5
kuro96oruk <kuroki@siba-service.jp>
2023-11-18 17:58:18 +0900

    :sparkles: 2.6.2まで追記

---
+++ src/chapter2/mock/chohan.test.ts
@@ -0,0 +1,20 @@
+import {chohan} from "./chohan";
+
+jest.mock("./seed", () => {
+  return {
+    seed: jest
+      .fn()
+      .mockImplementationOnce(() => 2)
+      .mockImplementationOnce(() => 1),
+  };
+});
+
+describe("chohan", () => {
+  it("returns even when seed returns an even number like 2", () => {
+    expect(chohan()).toBe("even");
+  });
+
+  it("returns odd when seed returns an odd number like 1", () => {
+    expect(chohan()).toBe("odd");
+  });
+});
